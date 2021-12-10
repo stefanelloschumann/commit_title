d8b67d0b732695b84ce37140bcc4416233cfef70
Stefanello <eduardo.stefanello@schumann.com.br>
2021-12-10 10:23:40 -0300

    6.36

--- branch.js
+++ branch.js
@@ -18,3 +18,30 @@ if (cf_20 != undefined && cf_20.lastChild != undefined && cf_20.lastChild.innerT
     cf_20.lastChild.innerText = ''
     cf_20.lastChild.appendChild(link)
 }
+
+let cf_29_s = document.getElementsByClassName('cf_29 string')
+
+for (i in cf_29_s) {
+    cf_29 = cf_29_s[i]
+    if (cf_29 != undefined && cf_29.lastChild != undefined) {
+        let link = document.createElement('a')
+        link.href = 'https://github.com/schumannlabs/protheus/tree/' + cf_29.innerText.replace('#', '%23')
+        link.innerText = cf_29.innerText
+        cf_29.innerText = ''
+        cf_29.appendChild(link)
+    }
+}
+
+
+let cf_20_s = document.getElementsByClassName('cf_20 int')
+
+for (i in cf_20_s) {
+    cf_20 = cf_20_s[i]
+    if (cf_20 != undefined && cf_20.lastChild != undefined) {
+        let link = document.createElement('a')
+        link.href = 'https://schumann.movidesk.com/Ticket/Edit/' + cf_20.innerText
+        link.innerText = cf_20.innerText
+        cf_20.innerText = ''
+        cf_20.appendChild(link)
+    }
+}
--- task.js
+++ task.js
@@ -10,7 +10,7 @@ head.appendChild(link);
 let issue = window.location.href.split(/([0-9]{4,5})/)[1]
 
 var iframe = document.createElement('iframe')
-iframe.src = 'https://script.google.com/a/macros/schumann.com.br/s/AKfycbyozfcPm70FMkZ5RhHluoX2QgmqGeZJ2E3UFmU75RcSl82E3eobQGK3JRe99Df_x4Td/exec' + '?issue=' + issue
+iframe.src = 'https://script.google.com/a/macros/schumann.com.br/s/AKfycbxFZG_kIcbjURCRoKsjVVv4gYXzYuap52XT1oOkEQDb2o7jvDUvuhlgdt1kQeeSO1XG/exec' + '?issue=' + issue
 iframe.style = 'height: 100%; width: 100%; border: 0px'
 
 var div = document.createElement('div')
@@ -18,22 +18,40 @@ div.id = '9381ccc9bdc24623b3d58941bc67df44'
 div.appendChild(iframe)
 
 var sidebar = document.getElementById('sidebar')
-chrome.storage.sync.get(['height', 'width'], function(items){
-    if (items.height == undefined)
-        items.height = 800
 
-    if (items.width == undefined)
-        items.width = 350
+if (issue != undefined)
+    chrome.storage.sync.get(['height', 'width'], function(items){
+        if (items.height == undefined)
+            items.height = 800
 
-    div.style = 'padding-top: 20px; height: ' + items.height + '; width: ' + items.width
-    sidebar.insertBefore(div, sidebar.firstElementChild)
-    $('#9381ccc9bdc24623b3d58941bc67df44').resizable({minHeight: 400, maxHeight: 1200, minWidth: 300, maxWidth: 400})
-});
+        if (items.width == undefined)
+            items.width = 350
+
+        div.style = 'padding-top: 20px; height: ' + items.height + '; width: ' + items.width
+        sidebar.insertBefore(div, sidebar.firstElementChild)
+        $('#9381ccc9bdc24623b3d58941bc67df44').resizable({minHeight: 400, maxHeight: 1200, minWidth: 300, maxWidth: 400})
+    })
+else
+    chrome.storage.sync.get(['height_u', 'width_u'], function(items){
+        if (items.height_u == undefined)
+            items.height_u = 100
+
+        if (items.width_u == undefined)
+            items.width_u = 350
+
+        div.style = 'padding-top: 20px; height: ' + items.height_u + '; width: ' + items.width_u
+        sidebar.insertBefore(div, sidebar.firstElementChild)
+        $('#9381ccc9bdc24623b3d58941bc67df44').resizable({minHeight: 50, maxHeight: 200, minWidth: 325, maxWidth: 375})
+    })
 
 var doit;
-function resizedw(div){chrome.storage.sync.set({'height': div.style.height, 'width': div.style.width}, function(){})}
+function resize_i(div){chrome.storage.sync.set({'height': div.style.height,   'width': div.style.width},   function(){})}
+function resize_u(div){chrome.storage.sync.set({'height_u': div.style.height, 'width_u': div.style.width}, function(){})}
 
 div.onresize = function() {
     clearTimeout(doit);
-    doit = setTimeout(function(){resizedw(div)}, 1000);
+    if (issue != undefined)
+        doit = setTimeout(function(){resize_i(div)}, 2000)
+    else
+        doit = setTimeout(function(){resize_u(div)}, 2000)
 };
